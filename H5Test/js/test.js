eb91c3404feaf342af73a15af85e00d46ca9dbe5
James <jingzl@hotmail.com>
2019-08-15 22:45:15 +0800

    some demo about js & dom

--- H5Test/js/test.js
+++ H5Test/js/test.js
@@ -16,4 +16,22 @@ printhello();
 
 
 
+function addnum( a, b ){
+	return a + b;
+}
+
+
+var arr = [10,20,30,40,50];
+var arr2 = arr.splice( 2, 1 );
+var arr3 = arr.splice( 2,2, 21,22,23);
+var arr4 = arr.splice( 2,2,[90,91,92]);
+console.log( arr );
+arr.reverse();
+console.log( arr );
+
+arr.sort();
+console.log( arr );
+
+
+
 
